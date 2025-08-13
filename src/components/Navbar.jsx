78cab50b6363067be92b2bdc37b2227e395d18c2
Vivek Jha <progamervivek2020@gmail.com>
2025-08-13 23:35:19 +0530

    for uploading on vercel for mobile view testing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="w-screen h-[20vh] md:h-[12vh] bg-red-400 fixed top-0 left-0 z-50">
+      <div style={{ fontFamily: "'Doto', sans-serif" }}></div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import "../index.css";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
-      <p className="mt-4">
-        This is the landing content. Scroll smoothly with Lenis.
-      </p>
-    </div>
+    <>
+      <Navbar />
+      <div className="text-center mt-40">
+        <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+        <p className="mt-4">
+          This is the landing content. Scroll smoothly with Lenis.
+        </p>
+      </div>
+    </>
   );
 }
 
